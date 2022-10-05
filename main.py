4941e35c16ce8cfefa7718956d676095a9b58ed5
EvanRothenbacher10 <825585@mystma.org>
2022-10-05 10:28:18 -0500

    2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-penguins = ["King", "Emperor", "Chinstrap", "Gentoo", "Little", "African", "Sourhtern Rockhopper", "Macaroni"]
+penguins = ["King", "Emperor", "Chinstrap", "Gentoo", "Little", "African", "Southern Rockhopper", "Macaroni"]
 factOptions = ["Habitat", "Diet", "Appearance", "Conservation Status"]
-
-def start(x):
-    print(penguins)
-start(input("Hello! Welcome to Penguin Facts! What Penguin would you like to learn about?"))
+import os
+def clear():     os.system('cls' if os.name == 'nt' else 'clear')
+clear()
+def start(x):    pass
+start(input("Hello! Welcome to Penguin Facts! What Penguin would you like to learn about?\n[ King, Emperor, Chinstrap, Gentoo, Little, African, Southern Rockhopper, Marcaroni ]\n"))
