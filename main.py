720c7b7af7d17455c27ef8baf34e7d7dd5bd8783
EvanRothenbacher10 <825585@mystma.org>
2022-10-05 09:42:52 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+penguins = ["King", "Emperor", "Chinstrap", "Gentoo", "Little", "African", "Sourhtern Rockhopper", "Macaroni"]
+factOptions = ["Habitat", "Diet", "Appearance", "Conservation Status"]
 
+def start(x):
+    print(penguins)
+start(input("Hello! Welcome to Penguin Facts! What Penguin would you like to learn about?"))
